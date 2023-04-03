7b4312452159f1999f4fb36b0fb52ae37837d0ad
Benjamin Attal <battal@rayleigh.wifi.local.cmu.edu>
2023-04-03 20:22:29 +0000

    Some minor config changes

---
+++ scripts/run_all_donerf_downsample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+LD_LIBRARY_PATH="" bash scripts/run_one_donerf_downsample.sh $1 barbershop $2 $3 $4
+LD_LIBRARY_PATH="" bash scripts/run_one_donerf_downsample.sh $1 pavillon $2 $3 $4
+LD_LIBRARY_PATH="" bash scripts/run_one_donerf_downsample.sh $1 bulldozer $2 $3 $4
+LD_LIBRARY_PATH="" bash scripts/run_one_donerf_downsample.sh $1 classroom $2 $3 $4
+LD_LIBRARY_PATH="" bash scripts/run_one_donerf_downsample.sh $1 forest $2 $3 $4
+LD_LIBRARY_PATH="" bash scripts/run_one_donerf_downsample.sh $1 sanmiguel $2 $3 $4
